31e6d2cf70e0b37fe75917c1cd531d4e6d352d3f
martin (darkmtbg2) <martin-baage@hotmail.com>
2021-05-11 08:44:26 +0200

    Add project files.

---
+++ TheEmergencyCentral/IEmergencyCentral.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheEmergencyCentral
+{
+    public interface IEmergencyCentral
+    {
+
+        /// <summary>
+        /// Queues the given number in the prirority List
+        /// </summary>
+        /// <param name="numberToQueue">The number calling</param>
+        /// <returns>Result if the number was queued</returns>
+        bool QueueNumber(int numberToQueue);
+
+
+        /// <summary>
+        /// Queues the number for an employee to call back to.
+        /// </summary>
+        /// <param name="numberToCall">The number to call</param>
+        /// <returns>If the number have been sucessfully registered</returns>
+        bool QueueNumberForCallFromEmployee(int numberToCall);
+
+        /// <summary>
+        /// When a number calls the Central on the priority Number
+        /// </summary>
+        /// <param name="numberCalling">number calling</param>
+        /// <returns>if the call is received and queued</returns>
+        bool PriorityCall(int numberCalling);
+
+        
+        
+
+    }
+}
